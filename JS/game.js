2b798ca4cbb639c8b5443124b3613dcecf902c19
Borja Caballero <borja.caba@gmail.com>
2018-07-09 15:17:39 +0200

    pantallas de transición

--- JS/game.js
+++ JS/game.js
@@ -1,9 +1,23 @@
+// Creación del constructor(clase)
+function Game(ctx) {
+  this.player = new Player()
+  this.ball = new Ball()
+  this.rows = 50;
+  this.columns = 50;
+  this.ctx = ctx;
+  this.doFrame()
+}
 
-// Creación de clase Game
-function Game(options) {
-  this.player = options.player;
-  this.ball;
-  this.rows = options.rows;
-  this.columns = options.columns;
-  this.ctx = options.ctx;
+//Creación del método dentro del constructor Game
+Game.prototype._drawBoard = function () {
+  for (var columnIndex = 0; columnIndex < this.columns; columnIndex++) {
+    for (var rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+      this.ctx.fillStyle = "#E3D4AB";
+      this.ctx.fillRect(columnIndex * 10, rowIndex * 10, 10, 10);
+    }
+  }
+}
+
+Game.prototype.doFrame = function () {
+    this._drawBoard();
 }
--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,48 @@
 
-
-$(document).ready(function(){
-  var game;
-
-  var canvas = document.getElementById("footballboard");
-  var ctx = canvas.getContext("2d");
-
-  ctx.beginPath();
-  ctx.rect(20, 40, 50, 50);
-  ctx.fillStyle = "#FF0000";
-  ctx.fill();
-  ctx.closePath();
-
-
-  game = new Game({
+function main() {
+  var gameContainer = document.getElementById("game-container")
+  buildSplash()
   
-  });
-  game.start();
+  function buildSplash() {
+    var btn = document.createElement("button");
+    btn.innerText = "Start Game"
+    gameContainer.appendChild(btn)
+    btn.addEventListener("click", destroySplash)
+  }
 
-})
+  function destroySplash() {
+    var btn = document.querySelector("button")
+    btn.remove();
+    buildGame()    
+  }
+
+  function buildGame() {
+    var canvas = document.createElement("canvas")
+    canvas.setAttribute("width", "500");
+    canvas.setAttribute("height", "500");
+    gameContainer.appendChild(canvas)
+    var ctx = canvas.getContext("2d");
+    var game = new Game(ctx);
+  }
+  
+  function destroyGame() {
+    var canvas = document.querySelector("canvas")
+    canvas.remove()
+    buildGameOver()
+  }
+
+  function buildGameOver() {
+    var btn = document.createElement("button");
+    btn.innerText = "Restart Game"
+    gameContainer.appendChild(btn)
+    btn.addEventListener("click", destroyGameOver)
+  }
+
+  function destroyGameOver() {
+    var btn = document.querySelector("button")
+    btn.remove();
+    buildGame();
+  }
+};
+
+window.addEventListener('load', main)
